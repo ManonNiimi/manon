29cdd7159ea8abdeeab8d0b377d6f8c8d0af303e
ManonNiimi <160984685+ManonNiimi@users.noreply.github.com>
2025-02-16 11:27:28 +0900

    Administrator

---
+++ Administrator/Administrator.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Administrator;
+import javax.swing.*;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Administrator extends javax.swing.JFrame {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        login_page login = new login_page();
+        login.setVisible(true);
+    }
+        
+}
